afbb6df8b5192ef9449f49225f7278db9f9788e5
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-21 21:56:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
 # Write your code here.
-def line (katz_deli)
-  katz_deli = []
-  case katz_deli.length
-    when 0
-    puts "The line is currently empty."
-  end 
+#def line (katz_deli)
+ # katz_deli = []
+  #case katz_deli.length
+   # when 0
+   ## end 
 end
 
   #case other_deli.length

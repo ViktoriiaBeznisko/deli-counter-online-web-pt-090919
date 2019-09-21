0f2037a32ccc0e85fb7c3f8bbd70952d25dacf40
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-21 22:26:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,7 @@
 
 def line (katz_deli)
- 
-  case katz_deli.length
-   when 0
-     puts "The line is currently empty."
+   if katz_deli.length
+   puts "The line is currently empty."
    end 
 end
 

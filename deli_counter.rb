8b8074ba9d4fa5b22887df1ab79a0773a75b54ca
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-21 21:27:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def line 
+  katz_deli = []
+  case katz_deli.length
+    when 0
+    puts "The line is currently empty."
+  end 
+end
+
+  case other_deli.length
+    when 
+      counter > 0
+      puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  end
+end

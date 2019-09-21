8bd67937600bd4cad0382665512ed49899f40137
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-21 23:34:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ def take_a_number (katz_deli_line, name)
 katz_deli_line << name
 end
 
-def now_serving (line, name)
+def now_serving (line)
   if line.length == 0
   puts "There is nobody waiting to be served!"
 else 

1f2514a9e11a23d0b68e446581d6fdd4fba6064b
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-21 22:22:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,15 @@
-# Write your code here.
-#def line (katz_deli)
- # katz_deli = []
-  #case katz_deli.length
-   # when 0
-   ## end 
-#end
 
-  #case other_deli.length
-    #when 
-     # counter > 0
-     # puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+def line (katz_deli)
+  katz_deli = []
+  case katz_deli.length
+   when 0
+     puts ""
+   end 
+end
+
 
 def take_a_number (katz_deli_line, name)
-puts "Welcome, #{name}. You are number #{katz_deli_line.length + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli_line.length + 1} in line."
 katz_deli_line << name
 end
 

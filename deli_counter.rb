f3fd5b74a589d051174904c57f6d388a9848aaca
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-21 22:05:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,11 +11,13 @@
      # counter > 0
      # puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
 
-def take_a_number (katz_deli, name = "Ada")
-  other_deli = []
-  counter = 1
-  katz_deli.each do |name|
-  other_deli.push(puts "Welcome, #{name}. You are number #{counter} in line.")
+def take_a_number (katz_deli, name)
+puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
   counter += 1
   end
+end
+
+def take_a_number(line, person)
+  puts "Welcome, #{person}. You are number #{line.size + 1} in line."
+  line << person
 end

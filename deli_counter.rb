0b4f3ed9e2c3c92b060e7cc06a2718284fcbabcb
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-21 22:00:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,8 +11,7 @@
      # counter > 0
      # puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
 
-def take_a_number (katz_deli)
-  katz_deli = []
+def take_a_number (katz_deli, name = "Ada")
   counter = 1
   katz_deli.each do |name|
   puts "Welcome, #{name}. You are number #{counter} in line."
